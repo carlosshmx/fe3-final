f65005a0354226608766e3e714d0a1b5509899b4
Carlos Colmenares <carlosshmx@hotmail.com>
2024-09-12 00:38:20 -0300

    prueba 3

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,6 +3,17 @@ import doctor from "/images/doctor.jpg"
 import { Link, useNavigate } from "react-router-dom";
 import {useDentistStates} from '../Components/utils/global.context'
 import img1 from "../../public/images/1.jpg"
+import img2 from "../../public/images/2.jpg"
+import img3 from "../../public/images/3.jpg"
+import img4 from "../../public/images/4.jpg"
+import img5 from "../../public/images/5.jpg"
+import img6 from "../../public/images/6.jpg"
+import img7 from "../../public/images/7.jpg"
+import img8 from "../../public/images/8.jpg"
+import img9 from "../../public/images/9.jpg"
+import img10 from "../../public/images/10.jpg"
+
+const profileImg = [img1,img2,img3,img4,img5,img6,img7,img8,img9,img10]
 
 
 const Card = ({ dentist }) => {
@@ -15,12 +26,9 @@ const Card = ({ dentist }) => {
     dispatch({ type: state.favs.some((fav) => fav.id == dentist.id) ? "DEL_DENTIST" : "ADD_DENTIS", payload: dentist });
   };
 
-  const imageUrl =  `/public/images/${dentist.id}.jpg`
-
-
   return (
     <div className="card" >
-        <img src={img1} alt="" />
+        <img src={profileImg[dentist.id-1]} alt="" />
         <i className={'fa-solid fa-star ' + (isFav ? 'checked' : '')} onClick={handleFav }></i>
         <Link to={`/dentist/${dentist.id}`}>{dentist.name}</Link>
         <p>User: {dentist.username}</p>
